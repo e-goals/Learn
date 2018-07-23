0c15e5bf5aa4a869019ecaa1ec881345e2474ccf
ez-goals <alanturing@yeah.net>
2018-07-23 18:25:12 +0800

    version 1.0

--- time-precision.aspx.cs
+++ time-precision.aspx.cs
@@ -2,27 +2,47 @@
 
 public partial class TimePrecision : System.Web.UI.Page
 {
-    protected string output = null;
+    protected string output = "";
+
+    private void WriteLine(string text)
+    {
+        output += text + "<br />";
+    }
+
+    private void WriteLine(string format, params object[] args)
+    {
+        output += string.Format(format + "<br />", args);
+    }
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        double precision1 = 0, precision2 = 0;
-        int times = 100;
-        for (int i = 0; i < times; i++)
+        if (!IsPostBack)
         {
-            precision1 += Test1();
-        }
+            double precision0 = 0, precision1 = 0, precision2 = 0;
+            int times = 100;
 
-        for (int i = 0; i < times; i++)
-        {
-            precision2 += Test2();
-        }
+            for (int i = 0; i < times; i++)
+            {
+                precision0 += Test0();
+            }
 
-        output = string.Format("System.DateTime.Now.Ticks: \t{0:0.0000} \tms<br />", precision1 / times);
-        output += string.Format("EzGoal.DateTime.Now.Ticks: \t{0:0.0} \tμs<br />", precision2 / times);
+            for (int i = 0; i < times; i++)
+            {
+                precision1 += Test1();
+            }
+
+            for (int i = 0; i < times; i++)
+            {
+                precision2 += Test2();
+            }
+
+            WriteLine("System.Environment.TickCount: {0:0.000} ms", precision0 / times);
+            WriteLine("System.DateTime.Now.Ticks: {0:0.0000} ms", precision1 / times);
+            WriteLine("EzGoal.DateTime.Now.Ticks: {0:0.0} μs", precision2 / times);
+        }
     }
 
-    private void Test0()
+    private void Test()
     {
         var beginTime = EasyGoal.Datetime.Now;
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
@@ -42,6 +62,17 @@ public partial class TimePrecision : System.Web.UI.Page
 
             System.Threading.Thread.Sleep(1000);
         }
+    }
+
+    private int Test0()
+    {
+        int t0 = Environment.TickCount;
+        int t1 = Environment.TickCount;
+        while (t0 == t1)
+        {
+            t1 = Environment.TickCount;
+        }
+        return (t1 - t0);
     }
 
     private double Test1()
@@ -65,4 +96,5 @@ public partial class TimePrecision : System.Web.UI.Page
         }
         return (t1 - t0) / 10.0;
     }
+
 }
