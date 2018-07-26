442f73b9181f32d4f9dbd36c5145de530a9c2770
ez-goals <alanturing@yeah.net>
2018-07-26 19:00:57 +0800

    version 1.0

---
+++ file-viewer.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="file-viewer.aspx.cs" Inherits="FileViewer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title></title>
+</head>
+<body>
+  <form id="_form" runat="server">
+    <div>
+    </div>
+  </form>
+</body>
+</html>
---
+++ file-viewer.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class FileViewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            ListFiles();
+        }
+    }
+
+    private void ListFiles()
+    {
+        string path = Server.MapPath("~/");
+        var ds = System.IO.Directory.GetDirectories(path);
+        DirectoryInfo di = new DirectoryInfo(path);
+
+        DirectoryInfo[] subDirs = di.GetDirectories();
+        foreach (DirectoryInfo subd in subDirs)
+        {
+            
+            Response.Write(subd.Name);
+            Response.Write("<br />");
+        }
+        Response.Write("<br />");
+        FileInfo[] fis = di.GetFiles();
+        foreach (FileInfo fi in fis)
+        {
+            Response.Write(fi.Name);
+            Response.Write("<br />");
+        }
+    }
+}
