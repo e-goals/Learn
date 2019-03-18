b807344d434c918fa17e55b7f35627f928dfae7d
ez-goals <alanturing@yeah.net>
2019-03-18 16:50:02 +0800

    Version 3.0

--- App_Code/Network.cs
+++ App_Code/Network.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Network 的摘要说明
 /// </summary>
-[WebService(Namespace = "http://tempuri.org/")]
+[WebService(Namespace = "http://ezgoals.net/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
 [System.Web.Script.Services.ScriptService]
